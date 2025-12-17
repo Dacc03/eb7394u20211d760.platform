47b6b6ccffce1563fa5991a62f93cc67feec3219
Dacc03 <u202120569@upc.edu.pe>
2025-12-16 20:57:04 -0500

    Update files 2.0

--- src/main/java/healthtrack/platform/u20211d760/Eb7394u20211d760Application.java
+++ src/main/java/healthtrack/platform/u20211d760/Eb7394u20211d760Application.java
@@ -2,8 +2,10 @@ package healthtrack.platform.u20211d760;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Eb7394u20211d760Application {
 
     public static void main(String[] args) {
