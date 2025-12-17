c2906da35a59b6bd43e36ce494e1b72f333c807f
Dacc03 <u202120569@upc.edu.pe>
2025-12-16 20:21:08 -0500

    Initial commit

---
+++ src/main/java/healthtrack/platform/u20211d760/Eb7394u20211d760Application.java
@@ -0,0 +1,13 @@
+package healthtrack.platform.u20211d760;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eb7394u20211d760Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eb7394u20211d760Application.class, args);
+    }
+
+}
---
+++ src/test/java/healthtrack/platform/u20211d760/Eb7394u20211d760ApplicationTests.java
@@ -0,0 +1,13 @@
+package healthtrack.platform.u20211d760;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eb7394u20211d760ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
